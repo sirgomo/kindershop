332c199cf1388e8360c6022423c3dc6894b8af15
sirgomo <wlodek_w@yahoo.de>
2023-04-11 19:22:03 +0200

    work on login page

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { BehaviorSubject } from 'rxjs';
+import { environments } from 'src/environments/environment';
+import { AppComponent } from '../app.component';
+import { MatDialogRef } from '@angular/material/dialog';
+import { LoginComponent } from '../login/login.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  app!: AppComponent;
+   api = environments.API_URL;
+   private tokenSubject : BehaviorSubject<string> = new BehaviorSubject('');
+   token$ = this.tokenSubject.asObservable();
 
-  constructor() { }
+  constructor(private httpClient: HttpClient, private snack: MatSnackBar) {}
+
+  login(data : {email : '', password: ''}, dilRef: MatDialogRef<LoginComponent>) {
+    this.httpClient.post(this.api + 'auth', data).subscribe()
+  }
+  logout() {}
+  setAppComponet(app: AppComponent) {
+    this.app = app;
+  }
 }
