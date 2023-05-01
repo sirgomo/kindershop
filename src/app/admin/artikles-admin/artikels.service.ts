5df36c31339ca86321bea26ec5f58c595320a88f
Warczachowski <wlodek_w@yahoo.de>
2023-05-01 19:18:33 +0200

    small refactoring

--- src/app/admin/artikles-admin/artikels.service.ts
+++ src/app/admin/artikles-admin/artikels.service.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
-import { Observable, combineLatest, map, of, scan, switchMap, take, tap } from 'rxjs';
+import { BehaviorSubject, Observable, combineLatest, map, of, scan, switchMap, take, tap } from 'rxjs';
 import { iArtikel } from 'src/app/model/iArtikel';
 import { environments } from 'src/environments/environment';
 import { AddEditArtikelComponent } from './add-edit-artikel/add-edit-artikel.component';
 import { CategoriesService } from '../categories/categories.service';
+import { iCategory } from 'src/app/model/icategory';
 
 
 @Injectable({
@@ -14,14 +15,17 @@ import { CategoriesService } from '../categories/categories.service';
 export class ArtikelsService {
 
   private API = environments.API_URL + 'artikel';
-  artikels$ = new Observable<iArtikel[]>();
-  constructor(private http: HttpClient, private catService: CategoriesService) { }
+  private artSubject: BehaviorSubject<iArtikel[]> = new BehaviorSubject<iArtikel[]>([]);
+  artikels$ = this.artSubject.asObservable();
+  categories$ = new Observable<iCategory[]>();
+  constructor(private http: HttpClient, private catService: CategoriesService) {
+    this.categories$ = this.catService.findAll();
+  }
 
   getAllArtikel(): Observable<iArtikel[]> {
     return this.http.get<iArtikel[]>(this.API).pipe(
-      map((res) => {
-        console.log(res)
-        this.artikels$ = of(res);
+      tap((res) => {
+        this.artSubject.next(res);
         return res;
       })
     );
@@ -34,42 +38,47 @@ export class ArtikelsService {
 
   createArtikel(artikel: iArtikel, dialRef: MatDialogRef<AddEditArtikelComponent>): Observable<any> {
     return this.http.post<iArtikel>(this.API, artikel).pipe(
-      map((res) => {
+      switchMap((res) => {
         if(res.id !== undefined && res.id !== null) {
-            this.artikels$ = combineLatest([this.artikels$, of([res])]).pipe(
-              map(([artikels, artikel]) => {
-                return [...artikels, ...artikel];
-              })
-            )
+          return this.artikels$.pipe(
+            map((artikels) => [...artikels, res])
+          );
+        } else {
+          return this.artikels$;
         }
-        return dialRef.close(this.artikels$);
+      }),
+      tap((newArtikels) => {
+        this.artSubject.next(newArtikels);
+        dialRef.close(newArtikels);
       })
     );
+
   }
 
   updateArtikel(artikel: iArtikel, dialRef: MatDialogRef<AddEditArtikelComponent>): Observable<any> {
 
-    return this.http.patch(`${this.API}/${artikel.id}`, artikel).pipe(
-      map(res => {
-        if(res === 1) {
-         this.artikels$ = combineLatest([this.artikels$, of(artikel), this.catService.findAll()]).pipe(
-            map(([arts, artikel, categories]) => {
-              return arts.map((item) => {
-                if( item.id === artikel.id) {
-                  item = artikel;
-                  const foundCategory = categories.find((c) => c.id == artikel.categories[0].id);
-                  if(foundCategory)
-                  item.categories = [foundCategory];
 
-                }
-                return item;
+      return this.http.patch(`${this.API}/${artikel.id}`, artikel).pipe(
+        map(res => {
+          if(res === 1) {
+           this.artikels$ = combineLatest([this.artikels$, of(artikel), this.categories$]).pipe(
+              map(([arts, artikel, categories]) => {
+                return arts.map((item) => {
+                  if( item.id === artikel.id) {
+                    item = artikel;
+                    const foundCategory = categories.find((c) => c.id == artikel.categories[0].id);
+                    if(foundCategory)
+                    item.categories = [foundCategory];
+
+                  }
+                  return item;
+                })
               })
-            })
-          )
-        }
-        return dialRef.close(this.artikels$);
-      })
-      );
+            )
+          }
+          return dialRef.close(this.artikels$);
+        })
+        );
   }
 
   deleteArtikel(id: number): Observable<iArtikel[]> {
@@ -82,4 +91,5 @@ export class ArtikelsService {
       })
     )
   }
+
 }
