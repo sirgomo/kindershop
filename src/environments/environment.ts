3a6ed5caa3769894104a8bb9e36671634a3240bd
Warczachowski <wlodek_w@yahoo.de>
2023-06-12 17:18:54 +0200

    api und migration zum https domain

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environments = {
  // API_URL : 'http://localhost:3000/',
- API_URL : 'http://165.22.23.164:3000/',
+ API_URL : 'https://kinder.ww-soft.de/api/',
 }
--- src/environments/environments.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environments = {
-  production: true,
-  API_URL : 'http://165.22.23.164:3000/',
-}
